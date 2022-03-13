05d352df561801492d0692f856e00fb03545c202
Christopher Wells <98454443+fullstacksoda@users.noreply.github.com>
2022-03-13 18:47:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// toggle mobile menu
+const toggleButton = document.getElementsByClassName('toggle')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
